a27054f9be92bdac55ab851d07b5d7f131075c4f
Gerhard Brueckl <gerhard@gbrueckl.at>
2022-06-10 12:27:54 +0200

    update libraries/dependencies

--- src/vscode/treeviews/connections/_types.ts
+++ src/vscode/treeviews/connections/_types.ts
@@ -20,13 +20,12 @@ export type SensitiveValueStore =
 	// Databricks Personal Access Token is stored in the VSCode Settings as plain text! 
 	// There is a potential risk that the PAT gets checked in to source control systems like git!
 	"VSCodeSettings"
-	
-	// (Experimental!) Databricks Personal Access Token is securely stored in the System Key Chain. 
+
 	// Easy Copying/Sharing connections with other users is not possible this way. 
-	// VSCode only stores a reference to the the KeyChain entry.",
+	// VSCode only stores a reference to the the Systems KeyChain entry.",
 	| "SystemKeyChain"
 
 	// (Experimental!) Databricks Personal Access Token is stored in an external Config file. 
-	// This is useful to manage all your PATs in one place and also easily share it
+	// This is useful to manage all your PATs in one place and also easily share them.
 	| "ExternalConfigFile"
 ;
