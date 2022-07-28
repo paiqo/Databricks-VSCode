bee8d34ee1241a1dab7a44819a0bddf3fc186fb7
Gerhard Brueckl <gerhard@gbrueckl.at>
2022-07-28 17:52:19 +0200

    fixed webpack packaging

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+    target: 'webworker', // vscode extensions run in webworker context for VS Code web 📖 -> https://webpack.js.org/configuration/target/#target
+
+    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    output: {
+        // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'extension.js',
+        libraryTarget: 'commonjs2',
+        devtoolModuleFilenameTemplate: '../[resource-path]'
+    },
+    devtool: 'source-map',
+    externals: {
+        vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    },
+    resolve: {
+        // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        alias: {
+            // provides alternate implementation for node module and source files
+        },
+        fallback: {
+            "https": false,
+            "http": false,
+            "path": false,
+            "assert": false,
+            "os": false,
+            "fs": false,
+            "util": false,
+            "url": false,
+            "async_hooks": false,
+            "tls": false,
+            "net": false
+        },
+        modules: [
+            './src',
+            './node_modules',
+            path.join(__dirname, 'node_modules')
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
+module.exports = config;
