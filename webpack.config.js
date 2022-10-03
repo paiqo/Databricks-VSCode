f4b187860e0ed519c81808b95f1fffbf1e7b93f4
Gerhard Brueckl <gerhard@gbrueckl.at>
2022-10-03 10:30:04 +0200

    initial setup to run as web-extension

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,73 @@
 'use strict';
 
 const path = require('path');
+const webpack = require("webpack");
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
+const webConfig = /** @type WebpackConfig */ {
+    context: __dirname,
+    mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+    target: "webworker", // web extensions run in a webworker context
+    entry: {
+        "extension-web": "./src/extension.ts" // source of the web extension main file
+    },
+    output: {
+        filename: "browser.js",
+        path: path.join(__dirname, "./dist/web"),
+        libraryTarget: "commonjs",
+    },
+    resolve: {
+        mainFields: ["browser", "module", "main"], // look for `browser` entry point in imported node modules
+        extensions: [".ts", ".js"], // support ts-files and js-files
+        alias: {
+            // provides alternate implementation for node module and source files
+        },
+        fallback: {
+            // Webpack 5 no longer polyfills Node.js core modules automatically.
+            // see https://webpack.js.org/configuration/resolve/#resolvefallback
+            // for the list of Node.js core module polyfills.
+            assert: require.resolve("assert"),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: "process/browser", // provide a shim for the global `process` variable
+        }),
+    ],
+    externals: {
+        vscode: "commonjs vscode", // ignored because it doesn't exist
+    },
+    performance: {
+        hints: false,
+    },
+    devtool: "nosources-source-map", // create a source map that points to the original source file
+};
+
 /** @type WebpackConfig */
-const extensionConfig = {
+const nodeConfig = {
+    context: __dirname,
     target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
     mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
     entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: {
         // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-        path: path.resolve(__dirname, 'dist'),
+        path: path.join(__dirname, "./dist/desktop"),
         filename: 'extension.js',
         libraryTarget: 'commonjs2'
     },
@@ -45,4 +99,4 @@ const extensionConfig = {
         level: "log", // enables logging required for problem matchers
     },
 };
-module.exports = [extensionConfig];
+module.exports = [nodeConfig, webConfig];
